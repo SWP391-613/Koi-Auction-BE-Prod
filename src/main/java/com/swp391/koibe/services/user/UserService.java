a0fd32c74c80d43edff25eb9aacc6be7abe5da2f
lcaohoanq <hoangclw@gmail.com>
2024-10-09 04:31:17 +0000

    Update from private repository - develop branch

--- src/main/java/com/swp391/koibe/services/user/UserService.java
+++ src/main/java/com/swp391/koibe/services/user/UserService.java
@@ -10,8 +10,8 @@ import com.swp391.koibe.enums.UserStatus;
 import com.swp391.koibe.exceptions.ExpiredTokenException;
 import com.swp391.koibe.exceptions.InsufficientFundsException;
 import com.swp391.koibe.exceptions.InvalidPasswordException;
-import com.swp391.koibe.exceptions.base.DataNotFoundException;
 import com.swp391.koibe.exceptions.PermissionDeniedException;
+import com.swp391.koibe.exceptions.base.DataNotFoundException;
 import com.swp391.koibe.models.Otp;
 import com.swp391.koibe.models.Role;
 import com.swp391.koibe.models.SocialAccount;
@@ -19,21 +19,21 @@ import com.swp391.koibe.models.User;
 import com.swp391.koibe.repositories.RoleRepository;
 import com.swp391.koibe.repositories.SocialAccountRepository;
 import com.swp391.koibe.repositories.UserRepository;
-
-import com.swp391.koibe.services.mail.MailService;
+import com.swp391.koibe.services.mail.IMailService;
 import com.swp391.koibe.services.otp.OtpService;
 import com.swp391.koibe.utils.MessageKeys;
 import com.swp391.koibe.utils.OTPUtils;
-
+import jakarta.mail.MessagingException;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
-
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -54,7 +54,7 @@ public class UserService implements IUserService {
     private final RoleRepository roleRepository;
     private final SocialAccountRepository socialAccountRepository;
     private final LocalizationUtils localizationUtils;
-    private final MailService mailService;
+    private final IMailService mailService;
     private final OtpService otpService;
 
     @Override
@@ -300,13 +300,36 @@ public class UserService implements IUserService {
         return userRepository.save(user);
     }
 
+    @Transactional
+    @Retryable(
+        retryFor = { MessagingException.class },  // Retry only for specific exceptions
+        maxAttempts = 3,                       // Maximum retry attempts
+        backoff = @Backoff(delay = 2000)       // 2 seconds delay between retries
+    )
     @Override
     public void updateAccountBalance(Long userId, Long payment) throws Exception {
         User existingUser = userRepository.findById(userId)
-                .orElseThrow(() -> new DataNotFoundException(
-                        localizationUtils.getLocalizedMessage(MessageKeys.USER_NOT_FOUND)
-                ));
+            .orElseThrow(() -> new DataNotFoundException(
+                localizationUtils.getLocalizedMessage(MessageKeys.USER_NOT_FOUND)
+            ));
         existingUser.setAccountBalance(existingUser.getAccountBalance() + payment);
+
+        Context context = new Context();
+        context.setVariable("name", existingUser.getFirstName());
+        context.setVariable("amount", payment);
+
+        try {
+            mailService.sendMail(
+                existingUser.getEmail(),
+                "Account balance updated",
+                EmailCategoriesEnum.BALANCE_FLUCTUATION.getType(),
+                context
+            );
+        } catch (MessagingException e) {
+            log.error("Failed to send email to {}", existingUser.getEmail(), e);
+            throw new MessagingException(String.format("Failed to send email to %s", existingUser.getEmail()));
+        }
+
         log.info("User {} balance updated. New balance: {}", userId, existingUser.getAccountBalance());
         userRepository.save(existingUser);
     }
